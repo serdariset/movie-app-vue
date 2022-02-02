2ea75fc31a67ec15432f82ad878858fe2660ed43
Mahmut Serdar İset <73692800+serdariset@users.noreply.github.com>
2022-02-02 13:25:05 +0300

    style: about page style

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
+  <div class="about-container">
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+export default {
+  name: "About"
+}
+</script>
+<style lang="scss">
+.about-container{
+  width: 100%;
+  min-height: calc(100vh - (5rem + 250px));
+  height: auto;
+  color: white;
+  display: flex;
+  justify-content: center;
+  padding-top: 3rem;
+}
+</style>
